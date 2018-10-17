ea7f78aa652d0dbeac7899d7ddfe1db28ec543ad
Gustavo Fonseca <gustavo@gfonseca.net>
2018-10-17 15:55:33 -0300

    Ajusta dados do release do pacote

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'thriftpy>=0.3.1',
-    'requests>=2.11.1',
-    'xylose>=1.31.0'
+    'thriftpy',
+    'requests',
+    'xylose',
 ]
-
-tests_require = []
 
 setup(
     name="citedbyapi",
@@ -16,15 +14,14 @@ setup(
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
     url="http://github.com/scieloorg/citedbyapi",
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
     ],
-    dependency_links=[],
-    tests_require=tests_require,
     test_suite='tests',
     install_requires=install_requires
 )
